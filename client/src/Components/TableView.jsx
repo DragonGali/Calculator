5af688d5eac49b3e8b520c52412e04288dd6a0f5
Gali Kertser <gali.kertser@gmail.com>
2025-09-10 11:27:55 +0300

    Added full table data

--- client/src/Components/TableView.jsx
+++ client/src/Components/TableView.jsx
@@ -4,7 +4,19 @@ import "../Styles/TableView.css"
 
 const TableView = ({ data, isFullTable = false}) => {
 
+  /* This is the TableView component, I use it twice in the application, when viewing
+     it from PathogenReduction and when opening the FullTable. This component helps display the
+     table data into a tabular format with a great presentation. Later on I plan to load the data from a database.
+
+     */
+
   const [selectedRowIndex, setSelectedRowIndex] = useState(null);
+
+  // Figure out which keys are dose values (exclude "name")
+  // So that the component will be dynamic and work with any amount of columns.
+  const doseKeys = data.length > 0 
+    ? Object.keys(data[0]).filter((key) => key !== "name")
+    : [];
 
 
   //Sets a row as selected
@@ -17,12 +29,17 @@ const TableView = ({ data, isFullTable = false}) => {
       <table className="fixed_headers">
         <tbody>
           {data.map((pathogen, index) => (
-            <tr key={index} className={selectedRowIndex === index ? 'selected' : ''}
-              onClick={() => handleRowClick(index)}>
+            <tr
+              key={index}
+              className={selectedRowIndex === index ? "selected" : ""}
+              onClick={() => handleRowClick(index)}
+            >
               <td className="pathogen-name">{pathogen.name}</td>
-              <td className="dose-value">{pathogen.oneLog}</td>
-              <td className="dose-value">{pathogen.twoLog}</td>
-              <td className="dose-value">{pathogen.threeLog}</td>
+              {doseKeys.map((doseKey, i) => (
+                <td key={i} className="dose-value">
+                  {pathogen[doseKey]}
+                </td>
+              ))}
             </tr>
           ))}
         </tbody>
--- client/src/data.js
+++ client/src/data.js
@@ -1,3 +1,4 @@
+import FullTable from "./Components/FullTable";
 import PathogenInactivation from "./Components/PathogenInactivation";
 import PathogenReduction from "./Components/PathogenReduction";
 import PlotFigures from "./Components/PlotFigures";
@@ -118,6 +119,26 @@ const data = {
                 { name: 'Streptococcus Faecalis', oneLog: 3.8, twoLog: 7.6, threeLog: 11.4 }
             ]
         },
+
+        FullTable: {
+            labels: [
+                "Pathogen Type" , "1-Log", "1.5-Log", "2-Log", "2.5-Log", "3-Log", "3.5-Log", "4-Log", "4.5-Log", "5-Log", "5.5-Log"
+            ],
+
+            tableData: [
+                { name: 'Aeromonas Handrohenus', oneLog: 1.0, onePointFiveLog: 1.5, twoLog: 2.0, twoPointFiveLog: 2.5, threeLog: 3.0, threePointFiveLog: 3.5, fourLog: 4.0, fourPointFiveLog: 4.5, fiveLog: 5.0, fivePointFiveLog: 5.5 },
+                { name: 'Aeromonas Handrophila', oneLog: 1.1, onePointFiveLog: 1.65, twoLog: 2.2, twoPointFiveLog: 2.75, threeLog: 3.3, threePointFiveLog: 3.85, fourLog: 4.4, fourPointFiveLog: 4.95, fiveLog: 5.5, fivePointFiveLog: 6.05 },
+                { name: 'Bacillus Anthracis', oneLog: 8.7, onePointFiveLog: 13.05, twoLog: 17.4, twoPointFiveLog: 21.75, threeLog: 26.1, threePointFiveLog: 30.45, fourLog: 34.8, fourPointFiveLog: 39.15, fiveLog: 43.5, fivePointFiveLog: 47.85 },
+                { name: 'Bacillus Cereus', oneLog: 1.8, onePointFiveLog: 2.7, twoLog: 3.6, twoPointFiveLog: 4.5, threeLog: 5.4, threePointFiveLog: 6.3, fourLog: 7.2, fourPointFiveLog: 8.1, fiveLog: 9.0, fivePointFiveLog: 9.9 },
+                { name: 'Bacillus Subtilis', oneLog: 2.2, onePointFiveLog: 3.3, twoLog: 4.4, twoPointFiveLog: 5.5, threeLog: 6.6, threePointFiveLog: 7.7, fourLog: 8.8, fourPointFiveLog: 9.9, fiveLog: 11.0, fivePointFiveLog: 12.1 },
+                { name: 'Campylobacter Jejuni', oneLog: 1.5, onePointFiveLog: 2.25, twoLog: 3.0, twoPointFiveLog: 3.75, threeLog: 4.5, threePointFiveLog: 5.25, fourLog: 6.0, fourPointFiveLog: 6.75, fiveLog: 7.5, fivePointFiveLog: 8.25 },
+                { name: 'Clostridium Perfringens', oneLog: 3.0, onePointFiveLog: 4.5, twoLog: 6.0, twoPointFiveLog: 7.5, threeLog: 9.0, threePointFiveLog: 10.5, fourLog: 12.0, fourPointFiveLog: 13.5, fiveLog: 15.0, fivePointFiveLog: 16.5 },
+                { name: 'E. Coli', oneLog: 3.0, onePointFiveLog: 4.5, twoLog: 6.0, twoPointFiveLog: 7.5, threeLog: 9.0, threePointFiveLog: 10.5, fourLog: 12.0, fourPointFiveLog: 13.5, fiveLog: 15.0, fivePointFiveLog: 16.5 },
+                { name: 'Enterococcus Faecalis', oneLog: 4.6, onePointFiveLog: 6.9, twoLog: 9.2, twoPointFiveLog: 11.5, threeLog: 13.8, threePointFiveLog: 16.1, fourLog: 18.4, fourPointFiveLog: 20.7, fiveLog: 23.0, fivePointFiveLog: 25.3 },
+                { name: 'Legionella Pneumophila', oneLog: 0.9, onePointFiveLog: 1.35, twoLog: 1.8, twoPointFiveLog: 2.25, threeLog: 2.7, threePointFiveLog: 3.15, fourLog: 3.6, fourPointFiveLog: 4.05, fiveLog: 4.5, fivePointFiveLog: 4.95 }
+            ]
+
+        }
 
         buttons : ["Display Tree View", "Display Table View", "Full Table"]
     },
