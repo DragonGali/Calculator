37b3db17211fc137f52b93c210673db6efa32452
DragonGali <gali.kertser@gmail.com>
2025-09-21 06:29:25 +0300

    quick push

--- client/src/Components/HODSystem.jsx
+++ client/src/Components/HODSystem.jsx
@@ -26,7 +26,77 @@ function HODSystem({ width, height, appState, sendUpdate}) {
         <p>HOD System</p>
       </div>
       <div className="wrapper">
-       
+        <div className="vertical-container">
+          
+          {/* Module */}
+          <div className="horizontal-container">
+            <div className="type-box">
+              <p>Module:</p>
+            </div>
+            <Dropdown
+              className="drop-down"
+              options={[...data.modules]}
+              height={Math.max(20, Math.min(height * 0.04, 64))}
+              width={Math.max(120, Math.min(width * 0.136, 280))}
+              placeholder={data.modules[0].label}
+              value={appState?.module} // controlled
+              onChange={(option) => { sendUpdate("HODSystem", "setModule", { module: option }) }}
+            />
+          </div>
+
+          {/* Model */}
+          <div className="horizontal-container">
+            <div className="type-box">
+              <p>Model:</p>
+            </div>
+            <Dropdown
+              className="drop-down"
+              options={[...data.model]}
+              height={Math.max(20, Math.min(height * 0.04, 64))}
+              width={Math.max(65, Math.min(width * 0.07, 280))}
+              placeholder={data.model[0].label}
+              value={appState?.model}
+              onChange={(option) => { sendUpdate("HODSystem", "setModel", { model: option }) }}
+            />
+            <Checkbox
+              items={[{ id: 1, text: 'Vertical', disabled: false }]}
+              className="checkbox"
+              checked={appState?.vertical}
+              onChange={(checked) => { sendUpdate("HODSystem", "setVertical", { vertical: checked }) }}
+            />
+          </div>
+
+          {/* Branch */}
+          <div className="horizontal-container">
+            <div className="type-box">
+              <p>Branch:</p>
+            </div>
+            <input
+              type="text"
+              className="simple-input"
+              value={appState?.branch ?? 1}
+              onChange={(e) => { sendUpdate("HODSystem", "setBranch", { branch: e.target.value }) }}
+            />
+            <div className="type-box">
+              <p>[Units]</p>
+            </div>
+          </div>
+
+          {/* Buttons */}
+          <div className="horizontal-container">
+            {data.HODButtons.map((button, index) => (
+              <div className="buttons" key={index}>
+                <div
+                  className={`button ${appState?.pressedButton === button ? 'IsPressed' : 'NotPressed'}`}
+                  onClick={() => handleClick(button)}
+                >
+                  <p>{button}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+
+        </div>
       </div>
     </div>
   )
