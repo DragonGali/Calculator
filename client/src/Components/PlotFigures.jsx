9c334a1991b59c577de5d6465af778284e888f07
DragonGali <gali.kertser@gmail.com>
2025-09-21 21:10:45 +0300

    PlotFigures done

--- client/src/Components/PlotFigures.jsx
+++ client/src/Components/PlotFigures.jsx
@@ -16,13 +16,13 @@ function PlotFigures ({unlockAll}) {
             <p>Plot Figures</p>
     </div>
     <div className="wrapper">
-        {/* <div className='vertical-container'>
+        <div className='vertical-container'>
                 {data.PlotFiguresButtons.map((button, index) => (
                     <div className='button' key={index} onClick={() => handleClick(button)}>
                             <p>{button}</p>
                     </div>
                 ))}
-            </div> */}
+            </div>
     </div>
     
     </div>
